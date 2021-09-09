315be8026dfd4ff7224d5e01f282e9fd3f6f8cd0
Anthony Ahumada <locopala99@gmail.com>
2021-09-09 10:16:40 +0200

    more js

--- frontend/src/app/task/unassign/unassign.component.ts
+++ frontend/src/app/task/unassign/unassign.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { TaskService } from "../../services/task.service";
+import { UserService } from "../../services/user.service";
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
+
 
 @Component({
   selector: 'app-unassign',
@@ -7,15 +14,55 @@ import { TaskService } from "../../services/task.service";
   styleUrls: ['./unassign.component.css']
 })
 export class UnassignComponent implements OnInit {
-  public userdata:any;
+  public userData:any;
+  public taskData:any;
+  public array2:any;
+  public name:any;
+  public registerData:any;
+  public message: string;
+  horizontalPosition: MatSnackBarHorizontalPosition = 'end';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
+  durationInSeconds: number = 2;
 
-  constructor(private _task:TaskService) { 
-    this.userdata = {}
+  constructor(private _task:TaskService , private _snackBar: MatSnackBar , private _userService: UserService) { 
+    this.message="";
+    this.userData={}
   }
 
   ngOnInit(): void {
+    this.getUserData()
   }
 
+  getUserData(){
+    this._userService.listUser().subscribe(
+      (res)=>{
+        this.userData = res.users;
+        
+        for (const iterator of this.userData) {
+          this.name = iterator
+          console.log(this.name);
+        }
+      }
+    )
+}
   
+
+  openSnackBarSuccesfull() {
+    this._snackBar.open(this.message, 'X', {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+      duration: this.durationInSeconds * 1000,
+      panelClass: ['style-snackBarTrue'],
+    });
+  }
+
+  openSnackBarError() {
+    this._snackBar.open(this.message, 'X', {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+      duration: this.durationInSeconds * 1000,
+      panelClass: ['style-snackBarFalse'],
+    });
+  }
 
 }
