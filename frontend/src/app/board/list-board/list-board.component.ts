281602126160d75b1da540cdaf5f9b2bc1c78ece
Anthony Ahumada <locopala99@gmail.com>
2021-09-14 12:20:13 +0200

    more js

--- frontend/src/app/board/list-board/list-board.component.ts
+++ frontend/src/app/board/list-board/list-board.component.ts
@@ -47,13 +47,13 @@ export class ListBoardComponent implements OnInit {
 
     mouseDrag: true,
 
-    touchDrag: true,
+    touchDrag: false,
 
     pullDrag: true,
 
-    dots: true,
+    dots: false,
 
-    navSpeed: 700,
+    navSpeed: 800,
 
     navText: ['', ''],
 
@@ -67,47 +67,31 @@ export class ListBoardComponent implements OnInit {
 
       400: {
 
-        items: 4
+        items: 2
 
       },
 
       740: {
 
-        items: 5
+        items: 3
 
       },
 
       940: {
 
-        items: 8
+        items: 5
 
       }
 
     },
 
-    nav: true
+    nav: false
 
   }
 
   
 
-    slides = [
-
-      {id: 1, img: "https://dummyimage.com/350x150/423b42/fff"},
-
-      {id: 2, img: "https://dummyimage.com/350x150/2a2b7a/fff"},
-
-      {id: 3, img: "https://dummyimage.com/350x150/1a2b7a/fff"},
-
-      {id: 4, img: "https://dummyimage.com/350x150/7a2b7a/fff"},
-
-      {id: 5, img: "https://dummyimage.com/350x150/9a2b7a/fff"},
-
-      {id: 6, img: "https://dummyimage.com/350x150/5a2b7a/fff"},
-
-      {id: 6, img: "https://dummyimage.com/350x150/4a2b7a/fff"}
-
-    ];
+    
 
  
   openSnackBarSuccesfull() {
