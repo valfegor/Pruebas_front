1e4fc74b5d9fe5120b48309e41c5aeb496b4333e
Anthony Ahumada <locopala99@gmail.com>
2021-09-14 11:47:32 +0200

    More js

--- frontend/src/app/board/list-board/list-board.component.ts
+++ frontend/src/app/board/list-board/list-board.component.ts
@@ -42,29 +42,72 @@ export class ListBoardComponent implements OnInit {
   }
 
   customOptions: OwlOptions = {
-    loop: true,
+
+    loop: false,
+
     mouseDrag: true,
+
     touchDrag: true,
+
     pullDrag: true,
-    dots: false,
+
+    dots: true,
+
     navSpeed: 700,
+
     navText: ['', ''],
+
     responsive: {
+
       0: {
+
         items: 1
+
       },
+
       400: {
+
         items: 2
+
       },
+
       740: {
+
         items: 3
+
       },
+
       940: {
+
         items: 4
+
       }
+
     },
+
     nav: true
+
   }
+
+  
+
+    slides = [
+
+      {id: 1, img: "https://dummyimage.com/350x150/423b42/fff"},
+
+      {id: 2, img: "https://dummyimage.com/350x150/2a2b7a/fff"},
+
+      {id: 3, img: "https://dummyimage.com/350x150/1a2b7a/fff"},
+
+      {id: 4, img: "https://dummyimage.com/350x150/7a2b7a/fff"},
+
+      {id: 5, img: "https://dummyimage.com/350x150/9a2b7a/fff"},
+
+      {id: 6, img: "https://dummyimage.com/350x150/5a2b7a/fff"},
+
+      {id: 6, img: "https://dummyimage.com/350x150/4a2b7a/fff"}
+
+    ];
 
  
   openSnackBarSuccesfull() {
