a42ceea0ed8df0ea0399e214cfc089a94e219d63
Anthony Ahumada <locopala99@gmail.com>
2021-09-16 13:36:20 +0200

    more js

--- frontend/src/app/admin/revokeassignment/revokeassignment.component.ts
+++ frontend/src/app/admin/revokeassignment/revokeassignment.component.ts
@@ -63,7 +63,9 @@ export class RevokeassignmentComponent implements OnInit {
   }
 
   listAssigned(){
-    this._taskService.UnassignTask(this.userData).subscribe(
+    console.log(this.registerData)
+    this._taskService.UnassignTask(this.registerData).subscribe(
+      
       (res)=>{
         console.log(res)
       }
