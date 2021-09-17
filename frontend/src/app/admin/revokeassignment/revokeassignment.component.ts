87cce7c540702292942cbdeeecd197dd256fc5a6
Anthony Ahumada <locopala99@gmail.com>
2021-09-17 13:23:34 +0200

    addint js

--- frontend/src/app/admin/revokeassignment/revokeassignment.component.ts
+++ frontend/src/app/admin/revokeassignment/revokeassignment.component.ts
@@ -2,7 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { TaskService } from "../../services/task.service";
 import { BoardService } from "../../services/board.service";
 import Swal from 'sweetalert2'
-
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-revokeassignment',
@@ -16,7 +20,10 @@ export class RevokeassignmentComponent implements OnInit {
   public taskData: any;
   public userData: any;
   public registerData: any;
-  constructor(private _taskService: TaskService , private _boardService: BoardService) {
+  horizontalPosition: MatSnackBarHorizontalPosition = 'end';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
+  durationInSeconds: number = 2;
+  constructor(private _taskService: TaskService , private _boardService: BoardService , private _snackBar: MatSnackBar) {
     this.boardData={};
     this.message=""
     this.search={};
@@ -31,42 +38,49 @@ export class RevokeassignmentComponent implements OnInit {
 
 
   alert(){
-    Swal.fire({
-      title: 'Are you sure you want to unsubscribe the task?',
-      text: 'The task will be assigned to the administrator and then assigned to a board member.',
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonText: 'Yes, unsubscribe!',
-      cancelButtonText: 'No, keep it'
-    }).then((result) => {
-      if (result.isConfirmed) {
-        this._taskService.UnassignTask(this.registerData).subscribe(
-          (res)=>{
-            Swal.fire(
-              'unsubscribed!',
-              'Your file has been unsubscribe.',
-              'success'
-            );
-            this.listAssigned();
-          },
-          (err)=>{
-            Swal.fire(
-              'Sorry you are not the Owner please contact the Owner',
-              'Cant unsubscribe the task',
-              'error'
-            );
-            console.log(err.error)
-          }
-        )
-        
-      } else if (result.dismiss === Swal.DismissReason.cancel) {
-        Swal.fire(
-          'Cancelled',
-          'You cancell the process have a nice day',
-          'error'
-        )
-      }
-    })
+    if (!this.registerData._idUser || !this.registerData._idtask) {
+      this.message = 'Failed process please check all the camps';
+      this.openSnackBarError();
+    } else {
+      Swal.fire({
+        title: 'Are you sure you want to unsubscribe the task?',
+        text: 'The task will be assigned to the administrator and then assigned to a board member.',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonText: 'Yes, unsubscribe!',
+        cancelButtonText: 'No, keep it'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          this._taskService.UnassignTask(this.registerData).subscribe(
+            (res)=>{
+              Swal.fire(
+                'unsubscribed!',
+                'Your file has been unsubscribe.',
+                'success'
+              );
+              this.listAssigned();
+            },
+            (err)=>{
+              Swal.fire(
+                'Sorry you are not the Owner please contact the Owner OR you are the admin please check the asignement module',
+                'Cant unsubscribe the task',
+                'error'
+              );
+              console.log(err.error)
+            }
+          )
+          
+        } else if (result.dismiss === Swal.DismissReason.cancel) {
+          Swal.fire(
+            'Cancelled',
+            'You cancell the process have a nice day',
+            'error'
+          )
+        }
+      })
+    }
+    
+    
   }
 
 
@@ -119,6 +133,24 @@ export class RevokeassignmentComponent implements OnInit {
         console.log(err.error)
       }
     )
+  }
+
+  openSnackBarSuccesfull() {
+    this._snackBar.open(this.message, 'X', {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+      duration: this.durationInSeconds * 1000,
+      panelClass: ['style-snackBarTrue'],
+    });
+  }
+
+  openSnackBarError() {
+    this._snackBar.open(this.message, 'X', {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+      duration: this.durationInSeconds * 1000,
+      panelClass: ['style-snackBarFalse'],
+    });
   }
   
 }
