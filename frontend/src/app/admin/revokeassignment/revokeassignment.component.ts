7d9c6b0c154e37b294247cf8f170c703543ff0a4
Anthony Ahumada <locopala99@gmail.com>
2021-09-17 10:26:25 +0200

    more js

--- frontend/src/app/admin/revokeassignment/revokeassignment.component.ts
+++ frontend/src/app/admin/revokeassignment/revokeassignment.component.ts
@@ -31,7 +31,31 @@ export class RevokeassignmentComponent implements OnInit {
 
 
   alert(){
-    Swal.fire('ALERT')
+    Swal.fire({
+      title: 'Are you sure?',
+      text: 'You will not be able to recover this imaginary file!',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: 'Yes, delete it!',
+      cancelButtonText: 'No, keep it'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire(
+          'Deleted!',
+          'Your imaginary file has been deleted.',
+          'success'
+        );
+        this.removeTask();
+      // For more information about handling dismissals please visit
+      // https://sweetalert2.github.io/#handling-dismissals
+      } else if (result.dismiss === Swal.DismissReason.cancel) {
+        Swal.fire(
+          'Cancelled',
+          'Your imaginary file is safe :)',
+          'error'
+        )
+      }
+    })
   }
 
 
